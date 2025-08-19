43657e243aa11995e87b5119494f9bb10a9df35b
achrafat <atiaachraf@gmail.com>
2025-08-19 15:44:20 +0200

    Use Response Model in endpoints

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ class Item(BaseModel):
     name: str
     price: float
     in_stock: bool = True
+class ItemResponse(BaseModel):
+    name: str
+    price: float
+    in_stock: bool    
 
 # In-memory "database"
 items: List[Item] = []
@@ -20,19 +24,19 @@ def read_root():
     return {"message": "Python MiniProject with FastAPI"}
 
 # List all items
-@app.get("/items/")
+@app.get("/items/",response_model=List[ItemResponse])
 def get_items():
     return items
 
 # Get an item by ID
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}",response_model=ItemResponse)
 def get_item(item_id: int):
     if item_id < 0 or item_id >= len(items):
         raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
 
 # Create a new item
-@app.post("/items/")
+@app.post("/items/",response_model=ItemResponse)
 def create_item(item: Item):
     items.append(item)
     return {"message": "Item added successfully", "item": item}
@@ -52,3 +56,4 @@ def delete_item(item_id: int):
         raise HTTPException(status_code=404, detail="Item not found")
     deleted_item = items.pop(item_id)
     return {"message": "Item deleted successfully", "item": deleted_item}
+g
